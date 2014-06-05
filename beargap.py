1e0f4d23c6cd054557f5f2fca938bcaccda01662
Sam Russell <sam.h.russell@gmail.com>
2014-06-06 00:46:30 +1200

    type in text, press generate, QR code gets generated

--- beargap.py
+++ beargap.py
@@ -6,29 +6,55 @@ from PIL import Image
 from PIL.ImageQt import ImageQt
 from PyQt4 import QtGui
 
+class BearGapWindow(QtGui.QWidget):
+  def __init__(self):
+    super(BearGapWindow, self).__init__()
+    self.initUI()
 
-def main():
+  def textToImg(self, text):
+    im = qrcode.make(text)
+    imq = ImageQt(im.convert("RGBA"))
+    self.pic.setPixmap(QtGui.QPixmap.fromImage(imq))
+    self.pic.adjustSize()
     
-    app = QtGui.QApplication(sys.argv)
 
-    w = QtGui.QWidget()
-    w.resize(450, 450)
-    w.move(300, 300)
-    w.setWindowTitle('Beargap')
+  def initUI(self):
+    
+
+    self.resize(450, 450)
+    self.move(300, 300)
+    self.setWindowTitle('Beargap')
     
     #im = Image.open("../garysaurus.png")
     im = qrcode.make("omg srsly :O")
     # need to convert to RGBA or else PIL.ImageQt fails
     imq = ImageQt(im.convert("RGBA"))
 
-    pic = QtGui.QLabel(w)
-    pic.setPixmap(QtGui.QPixmap.fromImage(imq))
+    self.pic = QtGui.QLabel(self)
+    self.pic.setPixmap(QtGui.QPixmap.fromImage(imq))
     #pic.setGeometry(0, 0, 290, 290)
 
-    w.show()
-    
-    sys.exit(app.exec_())
+    qle = QtGui.QLineEdit(self)
+    qle.textChanged[str].connect(self.onChanged)
 
+    qbtn = QtGui.QPushButton('Generate QR code', self)
+    qbtn.clicked.connect(self.onClicked)
+    qbtn.resize(qbtn.sizeHint())
+    qbtn.move(150, 0)
+
+    self.show()
+
+  def onClicked(self):
+    self.textToImg(self.text)
+
+  def onChanged(self, text):
+    self.text = text
+
+
+def main():
+  app = QtGui.QApplication(sys.argv)
+  bgw = BearGapWindow()
+  sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    main()
+  main()
